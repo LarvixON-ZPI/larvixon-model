2c0d0cd8cd73c196a75b5595b66b3841dccae7bf
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-10-18 11:07:05 +0200

    ethanol only detection

---
+++ test.py
@@ -0,0 +1,10 @@
+from train_real_data import extract_6_dishes_to_frame_folders
+from config import ROI_BOXES as roi_boxes
+from config import DISH_TO_CLASS as dish_to_class
+extract_6_dishes_to_frame_folders(
+    "L_RL_2025_09_25_49.mov",
+    "data/",
+    num_frames=150,
+    roi_boxes=roi_boxes,
+    dish_to_class=dish_to_class
+)
