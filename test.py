7d122192b36ebdf786fb415819e039a62f6c0f75
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-10-18 22:48:45 +0200

    more progress + slight refactor

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from train_real_data import extract_6_dishes_to_frame_folders
+from train_real_data import extract_8_dishes_to_frame_folders
 from config import ROI_BOXES as roi_boxes
 from config import DISH_TO_CLASS as dish_to_class
-extract_6_dishes_to_frame_folders(
+extract_8_dishes_to_frame_folders(
     "L_RL_EtOH_6.5_2025_09_25_49_2.mov",
     "data/",
     num_frames=200,
