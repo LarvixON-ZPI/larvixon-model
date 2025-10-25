96798d40f860043e801f7739bf4b5a074808c825
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-10-25 12:06:28 +0200

    finally fixed those damn roi boxes

--- scripts/test.py
+++ scripts/test.py
@@ -6,8 +6,8 @@ from src.config import ROI_BOXES as roi_boxes
 from src.config import DISH_TO_CLASS as dish_to_class
 
 extract_8_dishes_to_frame_folders(
-    "internal_data/L_RL_2025_09_22_48.mov",
+    "internal_data/output.mp4",
     "data/",
-    num_frames=50,
+    num_frames=15,
     dish_to_class=dish_to_class,
 )
